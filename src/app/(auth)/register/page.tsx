e7a00f60df8d374b865e11516feec231dbdb6d5c
MussaKhan74 <MarkeLoof@gmail.com>
2024-08-07 17:34:47 +0500

    add login page with UI

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,11 +1,40 @@
 "use client";
+// LIB IMPORTS
 import { useState } from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+
+// UI IMPORTS
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Calendar } from "@/components/ui/calendar";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function RegisterPage() {
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
+    const [suffix, setSuffix] = useState("");
+
+    const [date, setDate] = useState<Date>();
     const [phoneError, setPhoneError] = useState("");
     const [emailError, setEmailError] = useState("");
     const [firstNameError, setFirstNameError] = useState("");
@@ -92,235 +121,216 @@ export default function RegisterPage() {
         <div className="flex flex-col items-center justify-center	">
             <form action="">
                 {/* EMAIL FIELD */}
-                <div className="m-2 w-96 ">
-                    <label
-                        htmlFor="UserEmail"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Email*
-                    </label>
-
-                    <input
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="UserEmail">Email*</Label>
+                    <Input
                         type="email"
                         id="UserEmail"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
+                        placeholder="Email"
                         onChange={handleEmailChange}
                         required
                     />
                     {emailError && <p className="text-red-500">{emailError}</p>}
                 </div>
                 {/* FIRST NAME */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="fisrtName"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        First Name*
-                    </label>
-
-                    <input
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="fisrtName">First Name*</Label>
+                    <Input
                         type="text"
                         id="fisrtName"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
+                        placeholder="First Name"
                         onChange={handleFirstNameChange}
+                        required
+                    />
+                    {lastNameError && (
+                        <p className="text-red-500">{lastNameError}</p>
+                    )}
+                </div>
+                {/* LAST NAME */}
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="lastName">Last Name*</Label>
+                    <Input
+                        type="text"
+                        id="lastName"
+                        placeholder="First Name"
                         required
                     />
                     {firstNameError && (
                         <p className="text-red-500">{firstNameError}</p>
                     )}
                 </div>
-                {/* LAST NAME */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="lastName"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Last Name*
-                    </label>
-
-                    <input
-                        type="text"
-                        id="lastName"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
-                        required
-                    />
-                </div>
                 {/* DOB */}
                 <div className="m-2 w-96">
-                    <label
-                        htmlFor="dob"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        DOB - Date of Birth
-                    </label>
-
-                    <input
-                        type="text"
-                        id="dob"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
-                        required
-                    />
+                    <Label>Date of Birth*</Label>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button
+                                variant={"outline"}
+                                className={cn(
+                                    "w-96 justify-start text-left font-normal",
+                                    !date && "text-muted-foreground"
+                                )}
+                            >
+                                <CalendarIcon className="mr-2 h-4 text-[#72818E]" />
+                                {date ? (
+                                    format(date, "PPP")
+                                ) : (
+                                    <span className="text-[#72818E]">
+                                        Pick a date
+                                    </span>
+                                )}
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0">
+                            <Calendar
+                                selected={date}
+                                onSelect={setDate}
+                                initialFocus
+                                captionLayout="dropdown-buttons"
+                                fromYear={1960}
+                                toYear={2030}
+                            />
+                        </PopoverContent>
+                    </Popover>
                 </div>
                 {/* PHONE NO */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="phone"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Contact No
-                    </label>
-
-                    <input
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="phone">Contact No*</Label>
+                    <Input
                         type="number"
                         inputMode="numeric"
                         id="phone"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
+                        className="remove-arrow"
+                        placeholder="Contact No"
                         onChange={handlePhoneChange}
                         required
                     />
                     {phoneError && <p className="text-red-500">{phoneError}</p>}
                 </div>
+
                 {/* COUNTRY */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="country"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Country
-                    </label>
-
-                    <input
-                        type="text"
-                        id="country"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
-                        required
-                    />
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="country">Country*</Label>
+                    <Input type="text" id="country" placeholder="Country" />
                 </div>
+
                 {/* STREET */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="street"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Street
-                    </label>
-
-                    <input
-                        type="text"
-                        id="street"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
-                        required
-                    />
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="street">Street*</Label>
+                    <Input type="text" id="street" placeholder="Street" />
                 </div>
+
                 {/* CITY */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="city"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        City
-                    </label>
-
-                    <input
-                        type="text"
-                        id="city"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
-                        required
-                    />
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="city">City*</Label>
+                    <Input type="text" id="city" placeholder="City" />
                 </div>
+
                 {/* STATE */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="state"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        State
-                    </label>
-
-                    <input
-                        type="text"
-                        id="state"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
-                        required
-                    />
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="state">State*</Label>
+                    <Input type="text" id="state" placeholder="State" />
                 </div>
+
                 {/* POSTAL CODE */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="postal"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Postal Code
-                    </label>
-
-                    <input
-                        type="text"
+                <div className="m-2 grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="postal">Postal Code*</Label>
+                    <Input
+                        type="number"
+                        inputMode="numeric"
                         id="postal"
-                        className="mt-1 w-full rounded-md border-2 border-gray-500 shadow-sm sm:text-sm"
-                        required
+                        className="remove-arrow"
+                        placeholder="Postal Code"
                     />
                 </div>
+
                 {/* SUFFIX */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="suffix"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Suffix
-                    </label>
-
-                    <select
+                <div className="m-2">
+                    <Label htmlFor="suffix">Suffix</Label>
+                    <Select
                         name="suffix"
-                        id="suffix"
-                        className="mt-1.5 w-full rounded-lg py-[2px] border-2 border-gray-500 text-gray-700 sm:text-sm"
-                        required
+                        value={suffix}
+                        onValueChange={setSuffix}
                     >
-                        <option value="" disabled selected>
-                            Please select
-                        </option>
-                        <option value="he/him">He/Him</option>
-                        <option value="she/her">She/Her</option>
-                        <option value="they/them">They/Them</option>
-                    </select>
+                        <SelectTrigger className="w-96">
+                            <SelectValue placeholder="Select a Suffix" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Suffix</SelectLabel>
+                                <SelectItem value="he/him">He/Him</SelectItem>
+                                <SelectItem value="she/her">She/Her</SelectItem>
+                                <SelectItem value="they/them">
+                                    They/Them
+                                </SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
                 </div>
-                {/* SEX / GENER */}
-                <div className="m-2 w-96">
-                    <label
-                        htmlFor="gender"
-                        className="block text-xs font-medium text-gray-700"
-                    >
-                        Sex/Gender
-                    </label>
 
-                    <select
-                        name="gender"
-                        id="gender"
-                        className="mt-1.5 w-full rounded-lg py-[2px] border-2 border-gray-500 text-gray-700 sm:text-sm"
-                        required
+                {/* SEX / GENER */}
+                <div className="m-2">
+                    <Label htmlFor="gender">Sex/Gender</Label>
+                    <Select
+                        name="suffix"
+                        value={suffix}
+                        onValueChange={setSuffix}
                     >
-                        <option value="" disabled selected>
-                            Please select
-                        </option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
-                    </select>
+                        <SelectTrigger className="w-96">
+                            <SelectValue placeholder="Select a Gender / Sex" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>Gender/Sex</SelectLabel>
+                                <SelectItem value="male">Male</SelectItem>
+                                <SelectItem value="female">Female</SelectItem>
+                                <SelectItem value="other">Other</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
                 </div>
-                <div className="flex m-2">
-                    <input type="checkbox" className="h-5 w-5 mr-2" required />
-                    <div>
-                        <p>By creating an account you are agreeing to the</p>
-                        <p>Terms of Service & Privacy Policy.</p>
+
+                {/* PRIVACY POLICY */}
+                <div className="m-2 items-top flex items-center space-x-2">
+                    <Checkbox
+                        className="data-[state=checked]:bg-[#002D62]"
+                        id="terms"
+                        required
+                    />
+                    <div className="grid gap-1.5 leading-none">
+                        <label
+                            htmlFor="terms"
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                            Accept terms and conditions
+                        </label>
                     </div>
                 </div>
+                <p className="m-2 text-sm text-muted-foreground">
+                    You agree to our
+                    <Link
+                        className="mx-1 text-[#002D62] hover:text-[#000335] font-bold"
+                        href="https://www.fca.org/aboutus/privacy-policy"
+                    >
+                        Terms of Service
+                    </Link>
+                    and
+                    <Link
+                        className="mx-1 text-[#002D62] hover:text-[#000335] font-bold"
+                        href="https://www.fca.org/aboutus/privacy-policy"
+                    >
+                        Privacy Policy.
+                    </Link>
+                </p>
 
+                {/* SUBMIT BUTTON */}
                 <div className="flex items-center justify-center m-2 w-96">
-                    <button
+                    <Button
                         type="submit"
-                        className="inline-block rounded bg-blue-600 px-[158px] py-3 text-sm font-medium text-white hover:bg-blue-500 hover:shadow-xl focus:outline-none focus:ring active:bg-blue-500"
+                        className="inline-block rounded bg-[#002D62] px-[168px] text-sm font-medium text-white hover:bg-[#000335] focus:outline-none focus:ring active:bg-[#000335]"
                     >
                         Submit
-                    </button>
+                    </Button>
                 </div>
             </form>
         </div>
